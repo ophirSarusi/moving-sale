82caa30ee5eb769815e00131c425eadc4fce14d6
Ophir <osarusi@gmail.com>
2026-07-18 19:48:36 +0300

    Remove temporary dialog script override

--- dialog-stability-fix.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-// The reserved state is handled by the CTA and its contextual WhatsApp
-// message. Do not inject additional dialog content into the observed DOM.
-updateReservedDialogNote = function disableReservedDialogNote() {};
